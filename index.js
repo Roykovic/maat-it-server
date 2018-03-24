cc5b965435dfdbd5be160b7d08eabb34c2b939eb
Roykovic <Roy@isnecfan.nl>
2018-03-24 22:13:59 +0100

    Express setup

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+    res.send("You just performed a get on the maat-it server");
+});
+
+app.post('/', function(req, res){
+    res.send("You just performed a post on the maat-it server");
+});
+
+app.listen(process.env.PORT || 3000);
