b9870e2f9d020dff5696db326bfce4d2540a5380
Roykovic <Roy@isnecfan.nl>
2018-03-25 20:46:24 +0200

    i kinda just copied that code without looking what was in it, didnt workout

--- index.js
+++ index.js
@@ -26,10 +26,8 @@ function handleError(req, res, statusCode, message){
     res.json(message);
 };
 
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/clients', require('./routes/clients')(handleError));
