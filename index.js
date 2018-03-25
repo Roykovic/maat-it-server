3a9a6c7576ec03a323990d3869edc7b41fcfe451
Roykovic <Roy@isnecfan.nl>
2018-03-25 12:51:16 +0200

    Added require for client schema

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+require('./models/client');
 
 app.get('/', function(req, res){
     res.send("You just performed a get on the maat-it server");
