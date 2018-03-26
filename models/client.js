46de8d72bf70f12f1e3eafc236cde492d6b266bd
Roykovic <Roy@isnecfan.nl>
2018-03-26 18:49:43 +0200

    deleted custom id

--- models/client.js
+++ models/client.js
@@ -3,8 +3,7 @@ var mongoose = require('mongoose');
 console.log('Iniializing client schema');
 
 var clientSchema = new mongoose.Schema({
-    id: { type: Number, unique: true},
-    heartRate: { type: String, required: true },
+    heartRate: { type: String, required: true }
 }, {
     toJSON: { virtuals: true },
     toObject: { virtuals: true }
