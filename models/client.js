a0607b4230a25f83023e519f6808c3d7b40c2d37
Roykovic <Roy@isnecfan.nl>
2018-03-26 22:15:07 +0200

    removed id, mongoose does this automatically

--- models/client.js
+++ models/client.js
@@ -3,7 +3,6 @@ var mongoose = require('mongoose');
 console.log('Iniializing client schema');
 
 var clientSchema = new mongoose.Schema({
-    _id: { type: String },
     heartRate: { type: String, required: true }
 }, {
     toJSON: { virtuals: true },
