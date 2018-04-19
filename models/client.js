bdf3be773ca03dd98725b5c527345cdc49f7e830
Roykovic <roykovic97@outlook.com>
2018-04-19 11:23:10 +0200

    added anttendant id to client

--- models/client.js
+++ models/client.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 console.log('Iniializing client schema');
 
 var clientSchema = new mongoose.Schema({
-    heartRate: { type: String, required: true }
+    heartRate: { type: String, required: true },
+    attendantId: {type: mongoose.Schema.Types.ObjectId, ref: 'Attendant'}
 }, {
     toJSON: { virtuals: true },
     toObject: { virtuals: true }
