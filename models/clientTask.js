778afbe6bf1c9afac439fbb73826e6e0d590ef8d
Roykovic <roykovic97@outlook.com>
2018-05-23 18:25:12 +0200

    added junction table between client and task

---
+++ models/clientTask.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+console.log('Initializing clientTask schema');
+
+var clientTaskSchema = new mongoose.Schema({
+    clientId: {type: mongoose.Schema.Types.ObjectId, ref: 'Client'},
+    taskId: {type: mongoose.Schema.Types.ObjectId, ref: 'Task'},
+    startTime: { type: Date, required: false }
+
+}, {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+});
+
+mongoose.model('clientTask', clientTaskSchema);
