fc329fa045b27fad1855f2e249f127317975713a
Roykovic <roykovic97@outlook.com>
2018-06-26 12:07:39 +0200

    added create functionality for clientauth

--- routes/clientAuths.js
+++ routes/clientAuths.js
@@ -1,3 +1,23 @@
+var express = require('express');
+var _ = require('underscore');
+var router = express();
+var handleError;
+var jwt    = require('jsonwebtoken');
+
+var mongoose = require('mongoose');
+//mongoose.connect(config.mongooseUrl);
+mongoose.connect('mongodb://admin:admin@ds121309.mlab.com:21309/maat-it');
+ClientAuth = mongoose.model('ClientAuth');
+
+function createClientAuth(req, res) {
+    var qr = req.body.qr;
+    var clientAuth = new ClientAuth({ clientId: null , password: qr});
+    clientAuth.save(function (err, client) {
+        if (err) handleError(req, res, 500, err);
+        return res.json(client)
+    });
+}
+
 router.route('/')
     .post(createClientAuth);
 
