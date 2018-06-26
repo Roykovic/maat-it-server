eeedc93c4fd17ab4f0cb73117cccb8bd1461ccb1
Roykovic <roykovic97@outlook.com>
2018-06-26 22:46:28 +0200

    added check for null

--- routes/clients.js
+++ routes/clients.js
@@ -12,7 +12,7 @@ Client = mongoose.model('Client');
 
 function getHeartRate(req, res){
     Client.findById(req.params.id, function (err, client) {
-        if (err) handleError(req, res, 500, err);
+        if (err || !client) handleError(req, res, 500, err);
         return res.json(client.heartRate)
     })
 }
